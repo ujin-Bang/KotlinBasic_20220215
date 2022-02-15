0e1b666814c32203bd4a111742663e8d7ec01acb
ujin_bang <qkddnwls14@gmail.com>
2022-02-15 11:43:30 +0900

    else if 사용법

--- app/src/main/java/com/app40/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/app40/kotlinbasic_20220215/MainActivity.kt
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
 //            조건문 연습
 
-            val userAge = 17
+            val userAge = 15
 
 //            사용자의 나이가 20살 이상인가요? 맞으면 성인입니다. 맞으면 토스트로 띄우기
             if (userAge >= 20) {
@@ -62,9 +62,20 @@ class MainActivity : AppCompatActivity() {
 //                사용자 나이가 20살 이상일 때만 실행할 내용
                 Toast.makeText(this, "성인입니다.", Toast.LENGTH_SHORT).show()
             }
+            else if( userAge >= 17) {
+
+                Toast.makeText(this, "고등학생입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else if(userAge >= 14) {
+//                17살 미만. 14살 이상
+                Toast.makeText(this, "중학생입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else if(userAge >= 8) {
+                Toast.makeText(this, "초등학생입니다", Toast.LENGTH_SHORT).show()
+            }
             else{
-//                위의 질문이 틀렸을 때만 실행할 내용
-                Toast.makeText(this, "미성년자입니다.", Toast.LENGTH_SHORT).show()
+//                위의 모든 질문이 틀렸을 때만 실행할 내용
+                Toast.makeText(this, "미취학 아동입니다.", Toast.LENGTH_SHORT).show()
             }
         }
 
