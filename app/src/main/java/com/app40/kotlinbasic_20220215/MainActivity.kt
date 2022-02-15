d0ae0b28daa7781bc8e4a47028cc47be1fcbb5f3
ujin_bang <qkddnwls14@gmail.com>
2022-02-15 10:40:03 +0900

    토스트 띄우기

--- app/src/main/java/com/app40/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/app40/kotlinbasic_20220215/MainActivity.kt
@@ -17,10 +17,18 @@ class MainActivity : AppCompatActivity() {
 
 //            로그를 찍어보는 기능 실행. => btnLog가 클릭 될 때마다 실행.
             Log.d("메인화면","로그 버튼 눌림")
+            Log.d("메인화면","추가 로그 찍어보기")
+            Log.e("메인화면", "Log.e로 로그찍기_에러임을 알려줄때")
 
         }
 
-/
+        btnToast.setOnClickListener {
+//          토스트 버튼이 눌리면, Toast를 만들고(makeText) 보여주기(show)
+            Toast.makeText(this, "토스트 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show()
+            
+        }
+
+        
 
 
 
